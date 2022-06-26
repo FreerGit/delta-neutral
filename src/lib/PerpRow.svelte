df25b01d9b3dfff20c6015d9bbe141d5c7c9f2c2
FreerGit <fredrik-98@live.se>
2022-06-26 04:16:39 +0200

    feat: trade info for perpetuals

--- src/lib/PerpRow.svelte
+++ src/lib/PerpRow.svelte
@@ -27,19 +27,38 @@
 		<td>{row_info.fut_price}</td>
 		<td>{row_info.spot_price}</td>
 		<div class="container py-3">
-			<label for="my-modal-4" class="btn btn-sm btn-outline modal-button"
+			<label for={row_info.name} class="btn btn-sm btn-outline modal-button"
 				>Details</label
 			>
-			<input type="checkbox" id="my-modal-4" class="modal-toggle" />
-			<label for="my-modal-4" class="modal cursor-pointer">
+			<input type="checkbox" id={row_info.name} class="modal-toggle" />
+			<label for={row_info.name} class="modal cursor-pointer">
 				<label class="modal-box relative" for="">
-					<h3 class="text-lg font-bold">
-						Congratulations random Interner user!
-					</h3>
-					<p class="py-4">
-						You've been selected for a chance to get one year of subscription to
-						use Wikipedia for free!
-					</p>
+					<h3 class="text-xl font-bold">How to perform this basis trade:</h3>
+					{#if row_info.funding_rate < 0.0}
+						<p class="">tldr: margin short spot, long the perpetual</p>
+						<p class="py-4">
+							Since the funding rate is negative (The futures price is less than
+							the underlyings spot price) to collect funding you need to margin
+							short spot and long the perpetual. Then hold that position until
+							the funding rate becomes consistently positive.
+						</p>
+						<p class="underline">
+							note that no fees or margin payments were taken into account,
+							please take liquidation risk into account.
+						</p>
+					{:else}
+						<p class="">tldr: buy spot, short the perpetual</p>
+						<p class="py-4">
+							Since the funding rate is positive (The futures price is greater
+							than the underlyings spot price) to collect funding you need to
+							buy spot and short the perpetual. Then hold that position until
+							the funding rate becomes consistently negative.
+						</p>
+						<p class="underline">
+							note that no fees or margin payments were taken into account,
+							please take liquidation risk into account.
+						</p>
+					{/if}
 				</label>
 			</label>
 		</div>
